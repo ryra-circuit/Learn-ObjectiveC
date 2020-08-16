e0ccfbe45242c1d3317eaca88eab3a1310b44501
Dushan Saputhanthri <dushansrimantha@gmail.com>
2020-08-16 20:46:04 +0530

    Alerting added

--- Learn-ObjectiveC/VCs/Main/Home/HomeVC.m
+++ Learn-ObjectiveC/VCs/Main/Home/HomeVC.m
@@ -39,12 +39,15 @@
             break;
             
         case 3:
+            [self showAlertView];
             break;
             
         case 4:
+            [self showActionSheet];
             break;
             
         case 5:
+            [self showShareSheet];
             break;
             
         case 6:
@@ -66,5 +69,58 @@
             break;
     }
 }
+
+
+-(void)showAlertView {
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle: @"Alert" message: @"This is alert message" preferredStyle: UIAlertControllerStyleAlert];
+    
+    UIAlertAction *proceedAction = [UIAlertAction actionWithTitle: @"Proceed" style: UIAlertActionStyleDefault handler: nil];
+    
+    UIAlertAction *deleteAction = [UIAlertAction actionWithTitle: @"Delete" style: UIAlertActionStyleDestructive handler: nil];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle: @"Cancel" style: UIAlertActionStyleCancel handler: nil];
+    
+    [alertController addAction: proceedAction];
+    [alertController addAction: deleteAction];
+    [alertController addAction: cancelAction];
+    
+    [self presentViewController: alertController animated: YES completion: nil];
+}
+
+
+-(void)showActionSheet {
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle: @"Alert" message: @"This is alert message" preferredStyle: UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *proceedAction = [UIAlertAction actionWithTitle: @"Proceed" style: UIAlertActionStyleDefault handler: nil];
+    
+    UIAlertAction *deleteAction = [UIAlertAction actionWithTitle: @"Delete" style: UIAlertActionStyleDestructive handler: nil];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle: @"Cancel" style: UIAlertActionStyleCancel handler: nil];
+    
+    [alertController addAction: proceedAction];
+    [alertController addAction: deleteAction];
+    [alertController addAction: cancelAction];
+    
+    [self presentViewController: alertController animated: YES completion: nil];
+}
+
+
+-(void)showShareSheet {
+    
+    NSString *shareString = @"Test content";
+    UIImage *shareImage = [UIImage imageNamed:@"Test"];
+    NSURL *shareUrl = [NSURL URLWithString: @"https://www.google.com/" relativeToURL: nil];
+    
+    NSArray *activityItems = [NSArray arrayWithObjects: shareString, shareImage, shareUrl, nil];
+    
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems: activityItems applicationActivities: nil];
+    
+    activityViewController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+
+    [self presentViewController: activityViewController animated:YES completion:nil];
+}
+
 
 @end
